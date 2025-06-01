1541606d5d38a2c66ceb2e3980d6c2a17e5d92bd
Jaswanthchowdary18 <jaswanthchowdaryuppu2005@gmail.com>
2025-06-02 00:56:00 +0530

    fix import case, successful build

--- src/pages/Moviedetails/Moviedetails.jsx
+++ src/pages/Moviedetails/Moviedetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import './MovieDetails.css';
+import './Moviedetails.css';
 import {
     FaStar,
     FaRegClock,
